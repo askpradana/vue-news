7577e33731a078043888f69927bafea11264c9b6
naufal <askpradana@gmail.com>
2024-05-14 14:38:57 +0700

    feat: add finance

---
+++ src/components/NavbarMain.vue
@@ -0,0 +1,69 @@
+<script setup>
+const menuList = [
+    'Markets',
+    'Insights',
+    'News',
+    'Analysis',
+    'Charts',
+    'Brokers',
+    'Tools',
+    'Portofolio'
+];
+</script>
+
+<template>
+    <nav class="navbar">
+        <p>nfldyprdn.</p>
+        <span v-for="m in menuList" :key="m" class="listMenu">{{ m }}</span>
+        <div class="buttons">
+            <button>Signup</button>
+            <button class="buttons-disabled">Login</button>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+li {
+    list-style: none;
+}
+
+button {
+    background-color: white;
+    border: none;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+}
+
+.buttons :hover {
+    transform: scale(1.1);
+    transition: ease-in 0.2s;
+}
+
+.buttons {
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
+}
+
+.buttons-disabled {
+    background-color: transparent;
+    color: white;
+}
+
+.navbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: darkblue;
+    color: white;
+    height: 3rem;
+    padding: 0 3rem;
+}
+
+.listMenu {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NewsComponent from '../components/NewsComponent.vue'
 import FinanceComponent from '../components/FinanceComponent.vue'
+import NavbarMain from '../components/NavbarMain.vue'
 </script>
 
 <template>
   <main>
     <FinanceComponent />
+    <NavbarMain />
     <NewsComponent />
   </main>
 </template>
